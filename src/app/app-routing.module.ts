cc24a7c56467e28c90fa73fae4a2d3c483c9416b
farhanzebmalik <farhanzebmalik@insourcesolution.com>
2022-10-08 12:23:39 +0200

    implemented default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { JobAbroadComponent } from './job-abroad/job-abroad.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path: 'job-abroad',
+    component: JobAbroadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
