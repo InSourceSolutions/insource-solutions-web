88c2d3549e83b5660bacb055fa0827c93efed515
farhanzebmalik <farhanzebmalik@insourcesolution.com>
2022-10-14 12:46:41 +0200

    added navigation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,29 @@ $index: any;
 selected_image: any;
 selected_image_idx: any;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
   images = ['../../assets/Images/logos/logo.png'];
+
+  onHomeClick(): void {
+    this.router.navigate(['home'])
+  }
+
+  onAboutUsClick(): void {
+    this.router.navigate(['about-us'])
+  }
+
+  onServicesClick(): void {
+    this.router.navigate(['services'])
+  }
+
+  onContactUsClick(): void {
+    this.router.navigate(['contact-us'])
+  }
+
+  onJobAbroadClick(): void {
+    this.router.navigate(['job-abroad'])
+  }
 }
